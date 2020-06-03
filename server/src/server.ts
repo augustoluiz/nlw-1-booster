9b0d2ec52960ddb8b25c398d2128fc1457f0a57e
augustoluiz <augustogu7@yahoo.com.br>
2020-06-03 00:13:18 -0300

    Configurando o banco de dados

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,18 @@
+/*
+    start na aplicacao
+*/
+
+// request param --> obrigatorio
+// query param --> opcional (usado em filtros, paginação)
+// request body --> parâmetros para criação/atualização de informações
+
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
-app.get('/users', (req, res) => {
-    res.json([
-        'Aluno 1',
-        'Aluno 2',
-        'Aluno 3'
-    ])
-})
+app.use(routes)
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
-})
+}) 
