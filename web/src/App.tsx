7a639069270dbf898fabc78726530c1519c023a2
augustoluiz <augustogu7@yahoo.com.br>
2020-06-13 23:28:27 -0300

    Conceitos de JSK, Compoente, Propriedade e Estado do React

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 // JSX -> Sintaxe de XML dentro do JavaScript
@@ -6,10 +6,21 @@ import './App.css';
 import Header from './Header'
 
 function App() {
+
+  let [counter, setCounter] = useState(0) // [valor do estado, função para atualiazar o valor do estado]
+
+  function handleButtonClick(){
+    setCounter(counter++)
+  }
+
   return (
     <div>
-      <Header />
+      <Header title="Hello World"/>
+      
       <h1>Conteúdo da Aplicação</h1>
+      <h2>{counter}</h2>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
+    
     </div>
   );
 }
--- web/src/Header.tsx
+++ web/src/Header.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-function Header(){
+//React.FC => Function compenent, um componente escrito em forma de funcao
+
+//propriedade obrigatória, sem ponto de interrogação ex: title: string
+//propriedade não obrigatória, coloca o ponto de interrogação ex: title?: string
+
+interface HeaderProps {
+    title: string;
+}
+
+//O props é a propriedade, mas não necessáriamente precisa ter o nome de props
+//Estado => armazenar uma informação a partir do componente
+
+const Header: React.FC<HeaderProps> = (props) => {
     return(
         <header>
-            <h1>Ecoleta</h1>
+            <h1>{props.title}</h1>
         </header>
     )
 }
